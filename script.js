422e757c4eea8f4b38ca34bf2403f728f0af59a6
Alex Tomlinson <alexdangersky@gmail.com>
2016-05-01 19:41:28 -0400

    Spring cleaning

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ $(document).ready(function() {
     document.getElementById('audio').play();
   });
 });
+$(function(){
+		$(".element").typed({
+			strings: ["Click to begin"],
+			typeSpeed: 0,
+		});
+});
+function getLevenshteinenated(fob) {
+	// get distance
+	var distArray = levenshteinenator(fob.string_a.value, fob.string_b.value);
+	var dist = distArray[ distArray.length - 1 ][ distArray[ distArray.length - 1 ].length - 1 ];
+	// display distance
+	var percentage = dist/1347*100;
+	var score = (Math.round (percentage * 100) / 100);
+	var scoretwo = 100-score;
+	var scorethree = (Math.round (scoretwo * 100) / 100+"%");
+	fob.result.value = scorethree;
+
+	$('.finalscore p').text(scorethree);
+
+}
